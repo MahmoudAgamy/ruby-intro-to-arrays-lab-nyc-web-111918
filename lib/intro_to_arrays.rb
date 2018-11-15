f750f12e47d291a5d1d47c7329aa39f56d665ee2
Mahmoud Agamy <agamyv@gmail.com>
2018-11-15 09:16:18 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,14 +20,10 @@ def last_element(arr)
   return arr[arr.length]
 end
 
-def first_element(arr)
-  return arr[0]
+def first_element_with_array_methods(arr)
+  return arr.first
 end
 
-def third_element(arr)
-  return arr[2]
-end
-
-def last_element(arr)
-  return arr[arr.length]
+def last_element_with_array_methods(arr)
+  return arr.last
 end
